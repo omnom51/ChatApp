35cb6d5389baaf98374a202c164e29a4fca1ae2b
omnom51 <angied51121@gmail.com>
2024-06-09 13:05:31 +0200

    routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ChatComponent } from './chat/chat.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
