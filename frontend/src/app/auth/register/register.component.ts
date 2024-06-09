2088d17ef596f0ef1ea1c3ca2701e697b55b8369
omnom51 <angied51121@gmail.com>
2024-06-09 12:18:27 +0200

    register component

---
+++ frontend/src/app/auth/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatInputModule, MatButtonModule],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  register() {
+    this.authService.register(this.username, this.password).subscribe(() => {
+      this.router.navigate(['/login']);
+    });
+  }
+}
