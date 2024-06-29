10596501146577c62d509e2435ff5138da619b2f
fuszz <uliaszfilip@gmail.com>
2024-06-30 00:24:36 +0200

    Fix major bugs

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {provideHttpClient, withFetch} from "@angular/common/http";
+import {JWT_OPTIONS, JwtHelperService} from "@auth0/angular-jwt";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideHttpClient(
+    withFetch(),
+  ),
+  { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+  JwtHelperService,
+  provideRouter(routes),
+  provideClientHydration()]
 };
